c8db781c9fe5b320c719b319c7e8f25eefc1a9b3
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-04-26 12:13:27 -0700

    removed items from gitignore

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from app.src import create_app
 from app.src.models.models import User, db
 from werkzeug.security import generate_password_hash
@@ -51,61 +50,3 @@ def test_logout():
         response = client.get('/login/logged_out')
         assert response.status_code == 302
         assert b"/login" in response.data
-=======
-from app.src import create_app
-from app.src.models.models import User, db
-from werkzeug.security import generate_password_hash
-import pytest
-
-
-# test login page
-
-
-def test_login_page():
-    app = create_app()
-    # Create a test client using the Flask application configured for testing
-    with app.test_client() as client:
-        response = client.get('/login')
-        assert response.status_code == 200
-        assert b"Login" in response.data
-
-
-# test login credentials
-
-
-def test_login():
-    app = create_app()
-    app.app_context().push()
-    user = User(
-        firstname='Firstname',
-        lastname='Lastname',
-        email='admin@gmail.com',
-        password=generate_password_hash('admin123'),
-        authenticated=False
-    )
-    db.session.add(user)
-    db.session.commit()
-    with app.test_client() as client:
-        response = client.post(
-            '/login', json={'email': 'admin@gmail.com', 'password': 'admin123', 'remember': True}, content_type='application/json')
-        assert response.status_code == 200
-        assert b", logged in Succesfully" in response.data
-
-    user = User.query.get_or_404(db.session.query(
-        User.id).filter(User.email == 'admin@gmail.com').first()[0])
-    db.session.delete(user)
-    db.session.commit()
-
-# test logout
-
-
-def test_logout():
-    app = create_app()
-    with app.test_client() as client:
-        response = client.get('/login/logout')
-        assert response.status_code == 302
-        assert b"/login" in response.data
-
-
-# test new receipt
->>>>>>> 3a836a0811b06966706719eb9c306fd4b6c60f7e
