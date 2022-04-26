3ac3465bb4a2c4456049108318e7b9a8a73e5239
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-04-26 11:35:29 -0700

    updated logout endpoint

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 # using dotenv to retrieve .env variables
 load_dotenv()
 
+# factory
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -40,7 +42,8 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate = Migrate(app, db)
 
-    from .views import users, login, create_receipt, read, update_receipt, update_user, delete, receipts, totals
+    from .views import (users, login, create_receipt, read,
+                        update_receipt, update_user, delete, receipts, totals)
     app.register_blueprint(users.bp)
     app.register_blueprint(login.bp)
     app.register_blueprint(receipts.bp)
