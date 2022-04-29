89036647afa35f88346f364313978fd3e4a06cd0
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-04-29 09:03:21 -0700

    changed os var retrival method

--- app/app.py
+++ app/app.py
@@ -10,5 +10,5 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     app.run(threaded=True, host='0.0.0.0', port=port)
