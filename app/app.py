145c8ab456483a0db74db921fed535f824a07fc1
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-04-29 09:48:21 -0700

    tweaked host

--- app/app.py
+++ app/app.py
@@ -10,5 +10,5 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(threaded=True, host='0.0.0.0', port=port)
