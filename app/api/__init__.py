54d475d7ddc30caa0ee27a0f88aac1ca62e85566
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-04-30 07:39:32 -0700

    added secret key

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -13,8 +13,8 @@ load_dotenv()
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        # Using protected env varaibles for URI
+        # Using protected env varaibles
+        SECRET_KEY=os.getenv('SECRET_KEY'),
         # Dev uri
         # SQLALCHEMY_DATABASE_URI=f"postgresql://postgres@localhost/{os.getenv('POSTGRES_DB')}",
         # Heroku uri
