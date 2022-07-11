7b3b1d486e0ef7605244bd15af7487be04ee28d7
ZachGoldberg <zacharysgoldberg@outlook.com>
2022-07-11 12:57:16 -0700

    'create receipt' system test

--- app/tests/system/test_receipt.py
+++ app/tests/system/test_receipt.py
@@ -2,7 +2,7 @@ import os
 import json
 from datetime import datetime
 from api.models import User, Total, Receipt
-from ..base_test import BaseTest, _from, purchase_total, tax, address, items_services, transaction_number, cash, card_last_4, link, date, time
+from ..base_test import BaseTest, _from, purchase_total, tax, address, items_services, _get_cookie_from_response, transaction_number, cash, card_last_4, link, date, time
 from flask_jwt_extended import create_access_token
 
 
@@ -11,21 +11,17 @@ class TestReceipt(BaseTest):
         self.maxDiff = None
         with self.app() as client:
             with self.app_context():
-                client.post('/login/register', json={
+                client.post('/login/register', data={
                     'email': os.getenv('ADMIN'),
                     'password': os.getenv('MAIL_PASSWORD')
                 })
 
-                access_token = create_access_token(identity=os.getenv('ADMIN'))
-                headers = {
-                    "Authorization": "Bearer {}".format(access_token)}
-
-                client.post('/login', json={
+                auth_response = client.post('/login', data={
                     'email': os.getenv('ADMIN'),
                     'password': os.getenv('MAIL_PASSWORD')
-                }, headers=headers)
+                })
 
-                response = client.post('/users/home/receipts', json={
+                data = {
                     'from': _from,
                     'purchase_total': purchase_total,
                     'tax': tax,
@@ -37,21 +33,17 @@ class TestReceipt(BaseTest):
                     'link': link,
                     'date': json.dumps(datetime.strptime(date, "%Y-%m-%d"), indent=4, sort_keys=True, default=str),
                     'time': json.dumps(datetime.strptime(time, "%H:%M:%S"), indent=4, sort_keys=True, default=str)
-                }, headers=headers)
+                }
 
-                self.assertDictEqual({
-                    'from': _from,
-                    'purchase_total': purchase_total,
-                    'tax': tax,
-                    'address': address,
-                    'items_services': items_services,
-                    'transaction_number': transaction_number,
-                    'cash': cash,
-                    'card_last_4': card_last_4,
-                    'link': link,
-                    'date': json.dumps(datetime.strptime(date, "%Y-%m-%d"), indent=4, sort_keys=True, default=str),
-                    'time': json.dumps(datetime.strptime(time, "%H:%M:%S"), indent=4, sort_keys=True, default=str)
-                }, response.json)
+                csrf_token = _get_cookie_from_response(
+                    auth_response, "csrf_access_token")['csrf_access_token']
+                headers = {"X-CSRF-TOKEN": csrf_token}
+
+                response = client.post(
+                    '/users/home/receipts', json=data, headers=headers)
+
+                self.assertEqual(response.status_code, 200)
+                self.assertDictEqual(data, response.json)
 
     def test_delete_receipt(self):
         pass
